790c100272e1662a801f23622e320e4cb2e83489
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2013-12-13 14:11:56 +0900

    change jdbc driver com.microsoft/sqljdbc4 to net.sourceforge.jtds/jtds

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
     [org.apache.poi/poi "3.9"]
     [org.apache.poi/poi-ooxml "3.9"]
     [org.clojure/java.jdbc "0.3.0-alpha4"]
-    [com.microsoft/sqljdbc4 "3.0"]
+    ; [com.microsoft/sqljdbc4 "3.0"]
+    [net.sourceforge.jtds/jtds "1.2.4"]
   ]
   :aot [
     excelAsRDS.Dml
