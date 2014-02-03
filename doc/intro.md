68982852592e72e868c0a0e5545e78055e7e6e63
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2014-02-03 14:04:10 +0900

    Update Details

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to excelAsRDS
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
-(defproject excelAsRDS "0.0.1"
+(defproject excelAsRDS "0.0.3"
   :dependencies [
     [org.clojure/clojure "1.5.1"]
     [org.clojure/data.json "0.2.2"]
     [org.apache.poi/poi "3.9"]
     [org.apache.poi/poi-ooxml "3.9"]
-    [org.clojure/java.jdbc "0.3.0-alpha4"]
-    ; [com.microsoft/sqljdbc4 "3.0"]
-    [net.sourceforge.jtds/jtds "1.2.4"]
   ]
+  :plugins [[codox "0.6.6"]]
   :aot [
     excelAsRDS.Dml
     excelAsRDS.Utility
