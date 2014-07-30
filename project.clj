24141efbe8a454b941715f57c18eff4fc416c476
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2014-07-31 08:21:26 +0900

    Downgrade clojure.core 1.6.0 -> 1.5.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject excelAsRDS "0.0.1"
   :dependencies [
-    [org.clojure/clojure "1.6.0"]
+    [org.clojure/clojure "1.5.1"]
     [org.clojure/data.json "0.2.2"]
     [org.apache.poi/poi "3.9"]
     [org.apache.poi/poi-ooxml "3.9"]
